83be13a650c39baa769f4c3a3a31214a36ce3fbd
ibiscum <ulf.luecking@gmail.com>
2025-09-28 11:24:43 +0200

    ch04

--- Chapter04/fibonacci/fibotimes.js
+++ Chapter04/fibonacci/fibotimes.js
@@ -1,48 +1,44 @@
-
-import { fibonacciAsync } from './math';
-
 // Uncomment this section for synchronous Fibonacci
-/*
-for (var num = 1; num < 8000; num++) {
-    let now = new Date().toISOString();
-    console.log(`${now} Fibonacci for ${num} = ${math.fibonacci(num)}`);
-}
-/* */
+// import { fibonacci } from './math.js';
+
+// for (var num = 1; num < 50; num++) {
+//     let now = new Date().toISOString();
+//     console.log(`${now} Fibonacci for ${num} = ${fibonacci(num)}`);
+// }
 
 // Uncomment this section for an efficient synchronous Fibonacci
-/* 
-for (var num = 1; num < 8000; num++) {
-    let now = new Date().toISOString();
-    console.log(`${now} Fibonacci for ${num} = ${math.fibonacciLoop(num)}`);
-}
-/* */
+// import { fibonacciLoop } from './math.js';
 
+// for (var num = 1; num < 100; num++) {
+//     let now = new Date().toISOString();
+//     console.log(`${now} Fibonacci for ${num} = ${fibonacciLoop(num)}`);
+// }
 
 // Uncomment this section for an asynchronous Fibonacci
-/* */
-(async () => {
-    for (var num = 1; num < 8000; num++) {
-        await new Promise((resolve, reject) => {
-            fibonacciAsync(num, (err, fibo) => {
-                if (err) reject(err);
-                else {
-                    let now = new Date().toISOString();
-                    console.log(`${now} Fibonacci for ${num} = ${fibo}`);
-                    resolve();
-                }
-            })
-            
-        })
-    }
-})().catch(err => { console.error(err); });
-/* */
+// import { fibonacciAsync } from './math.js';
 
-/* Uncomment this for an asynchronous Fibonacci
+// (async () => {
+//     for (var num = 1; num < 8000; num++) {
+//         await new Promise((resolve, reject) => {
+//             fibonacciAsync(num, (err, fibo) => {
+//                 if (err) reject(err);
+//                 else {
+//                     let now = new Date().toISOString();
+//                     console.log(`${now} Fibonacci for ${num} = ${fibo}`);
+//                     resolve();
+//                 }
+//             })
+            
+//         })
+//     }
+// })().catch(err => { console.error(err); });
+
+// Uncomment this for an asynchronous Fibonacci
+import { fibonacciAwait } from './math.js';
 (async () => {
     for (var num = 1; num < 8000; num++) {
-        let fibo = await math.fibonacciAwait(num);
+        let fibo = await fibonacciAwait(num);
         let now = new Date().toISOString();
         console.log(`${now} Fibonacci for ${num} = ${fibo}`);
     }
 })().catch(err => { console.error(err); });
-*/
